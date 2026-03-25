1ab919363ca1275662eb5723c4d28bc3e70fe82e
Yaloha <ivan.chebotar1995@gmail.com>
2026-03-25 15:33:03 +0300

    removed redundant file, changed routing of link creation

--- backend/src/api/v1/links.py
+++ backend/src/api/v1/links.py
@@ -1,20 +1,19 @@
+import asyncpg
 from fastapi import APIRouter, Depends, HTTPException, status
-
-from src.api.deps import get_link_service
+from src.db.queries import LinkQueriesQueries
+from src.db.database import get_db
 from src.schemas.links import LinkCreate, LinkRead
 from src.services.links import LinkService
 
 
-router = APIRouter(
-    prefix='/links',
-    tags=['Links']
-)
+router = APIRouter()
 
-@router.post("/", response_model=LinkRead, status_code=status.HTTP_201_CREATED)
+@router.post("/create", response_model=LinkRead, status_code=status.HTTP_201_CREATED)
 async def create_link(
     payload: LinkCreate,
-    service: LinkService = Depends(get_link_service)
+    db: asyncpg.Connection = Depends(get_db)
 ):
+    service = LinkService(LinkQueriesQueries(db))
     try:
         new_link = await service.create_short_link(
             original_url=str(payload.original_url),

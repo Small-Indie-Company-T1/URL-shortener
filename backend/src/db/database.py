f03389e4954f0b26fb1694f321c837e3be94376b
Yaloha <ivan.chebotar1995@gmail.com>
2026-03-26 23:15:23 +0300

    db pool creation changes

--- backend/src/db/database.py
+++ backend/src/db/database.py
@@ -1,26 +1,10 @@
 import asyncpg
 asyncpg.Connection.exec = asyncpg.Connection.execute
-from src.core.config import settings
 from typing import AsyncGenerator
+from fastapi import Request
 
-pool = None
-
-async def create_db_pool():
-    global pool
-    pool = await asyncpg.create_pool(
-        dsn=settings.DATABASE_URL_SYNC,
-        min_size=5,
-        max_size=20
-    )
-
-async def close_db_pool():
-    global pool
-    if pool:
-        await pool.close()
-
-
-async def get_db() -> AsyncGenerator[asyncpg.Connection, None]:
-    global pool
+async def get_db(request: Request) -> AsyncGenerator[asyncpg.Connection, None]:
+    pool = request.app.state.pool
     if pool is None:
         raise Exception("db pool is not initialized")
 

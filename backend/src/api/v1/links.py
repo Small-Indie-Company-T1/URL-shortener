b11ef7c921fc926f7f0c2da654cbc92ef56652c9
Вячеслав Смирнов <smslava7@gmail.com>
2026-04-11 14:42:23 +0300

    service and endpoint of obtaining list of users links

--- backend/src/api/v1/links.py
+++ backend/src/api/v1/links.py
@@ -1,8 +1,10 @@
+import uuid
+
 import asyncpg
 from fastapi import APIRouter, Depends, HTTPException, status
 from src.db.queries import LinkQueriesQueries
 from src.db.database import get_db
-from src.schemas.links import LinkCreate, LinkRead
+from src.schemas.links import LinkCreate, LinkRead, LinkList
 from src.services.links import LinkService
 from src.api.deps import get_current_user
 
@@ -28,4 +30,22 @@ async def create_link(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Внутренняя ошибка"
         )
-    
+
+@router.get("/", response_model=LinkList)
+async def list_my_links(
+    db: asyncpg.Connection = Depends(get_db),
+    current_user = Depends(get_current_user)
+):
+    service = LinkService(LinkQueriesQueries(db))
+    try:
+        links = await service.get_user_links(user_id=current_user.id)
+        return {
+            "links": links,
+            "total": len(links)
+        }
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Внутренняя ошибка при получении списка"
+        )

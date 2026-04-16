8976d699da568d01e185fb417b331caf54582044
ArAKozlova <arakozlova@mai.education>
2026-04-16 22:01:11 +0300

    fix design

--- frontend/src/components/auth/PasswordInput.jsx
+++ frontend/src/components/auth/PasswordInput.jsx
@@ -1,31 +1,30 @@
 import { useState } from 'react';
 
 const PasswordInput = ({ password, placeholder, onChange, onBlur }) => {
-  const [showPassword, setShowPassword] = useState(false);
-  return (
-    <div>
-      <input
-        required
-        className="auth-input"
-        type={showPassword ? 'text' : 'password'}
-        placeholder={placeholder}
-        value={password}
-        onChange={onChange}
-        onBlur={onBlur}
-      />
-      <button
-        type="button"
-        onClick={() => setShowPassword(!showPassword)}
-        style={{
-          background: 'none',
-          border: 'none',
-          cursor: 'pointer',
-        }}
-      >
-        {showPassword ? '🙈' : '👁️'}
-      </button>
-    </div>
-  );
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <div className="password-input-wrapper">
+            <input
+                required
+                className="auth-input password-field"
+                type={showPassword ? 'text' : 'password'}
+                placeholder={placeholder}
+                value={password}
+                onChange={onChange}
+                onBlur={onBlur}
+            />
+            <button
+                type="button"
+                className="password-toggle-btn"
+                onClick={() => setShowPassword(!showPassword)}
+            >
+        <span className="material-symbols-outlined">
+          {showPassword ? 'password_2' : 'password_2_off'}
+        </span>
+            </button>
+        </div>
+    );
 };
 
 export default PasswordInput;
